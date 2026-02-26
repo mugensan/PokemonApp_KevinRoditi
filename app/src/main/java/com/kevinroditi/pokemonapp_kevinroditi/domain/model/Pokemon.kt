134606f6f3ea6741d9f61f7015c218832291f899
mugensan <kev.roditi@gmail.com>
2026-02-26 16:57:25 -0300

    Domain model added

--- app/src/main/java/com/kevinroditi/pokemonapp_kevinroditi/domain/model/Pokemon.kt
+++ app/src/main/java/com/kevinroditi/pokemonapp_kevinroditi/domain/model/Pokemon.kt
@@ -1,4 +1,22 @@
 package com.kevinroditi.pokemonapp_kevinroditi.domain.model
 
-class Pokemon {
-}
+/**
+ * Pokemon displayed in the HomeScreen
+ *
+ * Model intentionally minimal because it is used in a paginated list
+ *
+ * Responsability:
+ * - Provide clean UI-ready data
+ * - Be independant of API response structure
+ *
+ * - Used by Presentation Layer
+ *
+ * Performance considerations:
+ * - Lightweight for smooth scroll
+ * - Avoiding carrying heavy nested objects from API
+ */
+data class Pokemon(
+    val id: Int,
+    val name: String,
+    val imageUrl: String
+)
--- app/src/main/java/com/kevinroditi/pokemonapp_kevinroditi/domain/model/PokemonDetail.kt
+++ app/src/main/java/com/kevinroditi/pokemonapp_kevinroditi/domain/model/PokemonDetail.kt
@@ -1,4 +1,32 @@
 package com.kevinroditi.pokemonapp_kevinroditi.domain.model
 
-class PokemonDetail {
-}
+/**
+ * Domain model rep. detailed Pokemon info
+ *
+ * Responsability:
+ * - Provide clean UI-ready data
+ * - Be independant of API response structure
+ * - Abstract away complex DTO nesting
+ *
+ * - Created via mapper
+ * - Consumed by ViewModel and UI
+ */
+
+data class PokemonDetail(
+    val id: Int,
+    val name: String,
+    val imageUrl: String,
+    val height: Int,
+    val weight: Int,
+    val types: List<String>,
+    val stats: List<Stat>,
+    val abilities: List<String>
+)
+
+/**
+ * SEPERATING FOR CLARITY AND UI MAPPING
+ */
+data class stat(
+    val name: String,
+    val value: Int
+)
