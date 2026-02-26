2d8013d2241f970acdf0b40c3f3216a53418a82a
mugensan <kev.roditi@gmail.com>
2026-02-26 17:06:56 -0300

    Repository added

--- app/src/main/java/com/kevinroditi/pokemonapp_kevinroditi/domain/repository/PokemonRepository.kt
+++ app/src/main/java/com/kevinroditi/pokemonapp_kevinroditi/domain/repository/PokemonRepository.kt
@@ -1,4 +1,35 @@
 package com.kevinroditi.pokemonapp_kevinroditi.domain.repository
 
-class PokemonRepository {
+import androidx.paging.PagingData
+import com.kevinroditi.pokemonapp_kevinroditi.domain.model.Pokemon
+
+/**
+ * Repository contract for pokemon related orperations.
+ *
+ * Responsability:
+ * - define business required data ops
+ * - Remain independant of impl details
+ *
+ * Architecture:
+ * - Data layer implements this interface
+ * - UserCases depend on this interface
+ * - Presentation never talks directly to Data layer
+ *
+ * Why:
+ * - Enables testability
+ * - Enables swapping data sources (remote, local, cached)
+ * - Clean architecture boundaries repected
+ */
+
+
+
+interface PokemonRepository {
+
+    // @return Flow<PagingData<Pokemon>>
+    fun getPokemonPaging():
+            Flow<PagingData<Pokemon>>
 }
+
+//@Params name Pokemon name
+suspend fun getPokemonDetail(name: String):
+        Resource<PokemonDetail>
