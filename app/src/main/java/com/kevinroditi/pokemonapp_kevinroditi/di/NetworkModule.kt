eb11f51c6cc497def84e3ae47f0def2dae6d7db2
mugensan <kev.roditi@gmail.com>
2026-02-27 20:11:41 -0300

    Network wiring complete

--- app/src/main/java/com/kevinroditi/pokemonapp_kevinroditi/di/NetworkModule.kt
+++ app/src/main/java/com/kevinroditi/pokemonapp_kevinroditi/di/NetworkModule.kt
@@ -1,4 +1,79 @@
 package com.kevinroditi.pokemonapp_kevinroditi.di
 
-class NetworkModule {
+/**
+ * Provides network related dependencies
+ *
+ * Responsabilities:
+ * - Config. OkhttpClient
+ * - Config. Retrofit
+ * - Provide API service
+ *
+ * Architecture:
+ * - Installed in SingletonComponent to ensure single instance
+ *   accross entire app lifecycle
+ *
+ *   Why singleton:
+ *   - Retrofit and OkHttp are expensive to create
+ *   - They should be shared
+ */
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val BASE_URL = "https://pokeapi.co/api/"
+
+    // Provides HTTP logging interceptor
+    @Provides
+    @Singleton
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().apply{
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+    /** Provides OkHttp client
+     * Config:
+     * - 30s timeouts for reliability
+     * - Logging interceptor
+     */
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient{
+        return OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    /**
+     * Provides Retrofit instance
+     *
+     * Using Moshi for JSON parsing
+     * Lightweight and Kotlin friendly
+     */
+
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient
+    ): Retrofit{
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    }
+
+    /**
+     * Provides API service impl.
+     */
+    @Provides
+    @Singleton
+    fun providePokeApiService(
+        retrofit: Retrofit
+    ): PokeApiService{
+        return retrofit.create(PokeApiService::class.java)
+
 }
