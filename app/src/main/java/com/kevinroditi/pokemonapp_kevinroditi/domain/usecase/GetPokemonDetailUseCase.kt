594f76c032d0f77cb66b1c915ab6f7398da007eb
mugensan <kev.roditi@gmail.com>
2026-02-27 19:59:26 -0300

    Domain layer fully complete

--- app/src/main/java/com/kevinroditi/pokemonapp_kevinroditi/domain/usecase/GetPokemonDetailUseCase.kt
+++ app/src/main/java/com/kevinroditi/pokemonapp_kevinroditi/domain/usecase/GetPokemonDetailUseCase.kt
@@ -1,4 +1,21 @@
 package com.kevinroditi.pokemonapp_kevinroditi.domain.usecase
 
-class GetPokemonDetailUseCase {
+import com.kevinroditi.pokemonapp_kevinroditi.core.util.Resource
+import com.kevinroditi.pokemonapp_kevinroditi.domain.model.PokemonDetail
+
+/**
+ * UseCase responsible for retreiving detailed Pokemon info
+ *
+ * Responsabilities:
+ * - Encapsulate detail fetching logic
+ * - Provide single entry point for VM
+ */
+class GetPokemonDetailUseCase @Inject constructor(
+    private val repository: PokemonRepository
+) {
+    // Fetch Pokemon detail by name
+    suspend operator fun invoke(name: String):
+            Resource<PokemonDetail> {
+        return repository.getPokemonDetail(name)
+    }
 }
--- app/src/main/java/com/kevinroditi/pokemonapp_kevinroditi/domain/usecase/GetPokemonPagingUseCase.kt
+++ app/src/main/java/com/kevinroditi/pokemonapp_kevinroditi/domain/usecase/GetPokemonPagingUseCase.kt
@@ -1,4 +1,25 @@
 package com.kevinroditi.pokemonapp_kevinroditi.domain.usecase
 
-class GetPokemonPagingUseCase {
+/**
+ * UseCase responsible for retreiving paginated Pokemon list
+ *
+ * Why UseCase Layer:
+ * - Encapsulate business logic
+ * - Decouples ViewModel from Repository
+ * - Makes testing easier
+ *
+ * Architecture:
+ * - ViewModel depends on this
+ * - This depends on Repository
+ * - Repository depends on Data Layer
+ */
+
+class GetPokemonPagingUseCase @Inject constructor(
+    private val repository: PokemonRepository
+){
+
+    // Invoked by VM to obtain paginated Pokemon
+    operator fun invoke(): Flow<PagingData<Pokemon>>{
+        return repository.getPokemonPaging()
+    }
 }
