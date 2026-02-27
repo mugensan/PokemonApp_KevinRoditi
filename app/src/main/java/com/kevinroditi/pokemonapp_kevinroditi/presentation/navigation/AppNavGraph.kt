53ad0195b94bb1d1ab04aebe202d20b294e43888
mugensan <kev.roditi@gmail.com>
2026-02-27 20:58:54 -0300

    Nav added

--- app/src/main/java/com/kevinroditi/pokemonapp_kevinroditi/presentation/navigation/AppNavGraph.kt
+++ app/src/main/java/com/kevinroditi/pokemonapp_kevinroditi/presentation/navigation/AppNavGraph.kt
@@ -1,4 +1,58 @@
 package com.kevinroditi.pokemonapp_kevinroditi.presentation.navigation
 
-class AppNavGraph {
+import com.kevinroditi.pokemonapp_kevinroditi.presentation.detail.DetailScreen
+
+/**
+ * Central nav.
+ *
+ * Responsibilities:
+ * - Defining all routes in one place
+ * - Handle argument passing
+ * - Keep nav. scalable
+ *
+ * Why centralized?
+ * - Avoids magic strings across UI
+ * - Makes adding new screens easier
+ * - Improve readability
+ */
+
+@Composable
+fun AppNavGraph(
+    navController: NavHostController
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    )
+
+    // HomeScreen
+    composable(Screen.Home.route) {
+        HomeScreen(
+            onPokemonClick = { name ->
+                navController.navigate(Screen.Detail.createRoute(name))
+            }
+        )
+    }
+
+    // DetailScreen
+    // Receives Pokemon name as argument
+    composable(
+        route = Screen.Detail.route,
+        arguments = listOf(
+            navArgument(Screen.Detail.ARG_NAME) {
+                type = NavType.StringType
+            }
+        )
+    ) { backStackEntry ->
+        val name = backStackEntry.arguments?.getString(Screen.Detail.ARG_NAME)
+
+            ?: return@composable
+
+        DetailScreen(
+            name = name,
+            onBackClick = {
+                navController.popBackStack()
+            }
+        )
+    }
 }
--- app/src/main/java/com/kevinroditi/pokemonapp_kevinroditi/presentation/navigation/Screen.kt
+++ app/src/main/java/com/kevinroditi/pokemonapp_kevinroditi/presentation/navigation/Screen.kt
@@ -1,4 +1,20 @@
 package com.kevinroditi.pokemonapp_kevinroditi.presentation.navigation
 
-class Screen {
+/**
+ * Sealed class for def. nav. routes
+ *
+ * Why sealed class instead of raw string?
+ * - Type safety
+ * - Prevents typos
+ * - Centralized route management
+ */
+sealed class Screen(val route: String) {
+    object Home : Screen("home")
+    object Detail : Screen("detail/{name}") {
+        const val ARG_NAME = "name"
+        fun createRoute(name: String): String {
+            return "detail/$name"
+        }
+
+    }
 }
